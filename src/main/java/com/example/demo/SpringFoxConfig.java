679fd93a922b467fd11059e634ae1b44a5bd0ecd
Ava Wulkan <ava.wulkan@gmail.com>
2021-08-24 13:34:32 +0200

    lagt till dokumentation

---
+++ src/main/java/com/example/demo/SpringFoxConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfig {
+
+
+    //Url to api documentation :  v3/api-docs
+    //Swagger-ui : /swagger-ui/
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.any())
+                .build();
+    }
+}
