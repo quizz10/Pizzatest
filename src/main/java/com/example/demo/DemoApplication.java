9673d1de7da08334d38ade795ac66f2b6d4977fc
Johnny Olsen <johnny.olsen@iths.se>
2021-08-25 09:38:26 +0200

    Fixat stavfel och versaler :)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -19,12 +19,12 @@ public class DemoApplication {
 
         return args -> {
             if (pizzaRepository.count() == 0) {
-                pizzaRepository.save(new Pizza("Hawaii", 80, "Tomatsås, ost, Skinka, Ananas"));
-                pizzaRepository.save(new Pizza("Vesuvio", 80, "Tomatsås, ost, Skina"));
-                pizzaRepository.save(new Pizza("Mama Mia", 85, "Tomatsås, ost, Skinka, räkor"));
-                pizzaRepository.save(new Pizza("Quattro Staggioni", 100, "Tomatsås, ost, Skinka, Champinjoner, Räkor, Kronärtskocka"));
-                pizzaRepository.save(new Pizza("Maffia", 110, "Tomatsås, Ost, Oxfilé, Ägg, Bacon, Tabasco"));
-                pizzaRepository.save(new Pizza("SuperKebabPizza", 110, "Tomatsås, ost, Kebab, Sås, Pommes, Jalapenos, Sallad"));
+                pizzaRepository.save(new Pizza("Hawaii", 80, "Tomatsås, Ost, Skinka, Ananas"));
+                pizzaRepository.save(new Pizza("Vesuvio", 80, "Tomatsås, Ost, Skinka"));
+                pizzaRepository.save(new Pizza("Mama Mia", 85, "Tomatsås, Ost, Skinka, Räkor"));
+                pizzaRepository.save(new Pizza("Quattro Stagioni", 100, "Tomatsås, Ost, Skinka, Champinjoner, Räkor, Kronärtskocka"));
+                pizzaRepository.save(new Pizza("Mafia", 110, "Tomatsås, Ost, Oxfilé, Ägg, Bacon, Tabasco"));
+                pizzaRepository.save(new Pizza("Super KebabPizza", 110, "Tomatsås, Ost, Kebab, Sås, Pommes, Jalapenos, Sallad"));
             }
 
         };
