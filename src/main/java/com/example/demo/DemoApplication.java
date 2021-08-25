e0021b73ecf93ce4cdb037b5a7cddb7636ee6590
Johnny Olsen <johnny.olsen@iths.se>
2021-08-25 11:12:36 +0200

    Småfix

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -22,12 +22,11 @@ public class DemoApplication {
 
                 pizzaRepository.save(new Pizza("Hawaii", 80, "Tomatsås,Ost,Skinka,Ananas"));
                 pizzaRepository.save(new Pizza("Vesuvio", 80, "Tomatsås,Ost,Skinka"));
-                pizzaRepository.save(new Pizza("Mama Mia", 85, "Tomatsås,Ost,Skinka,Räkor"));
-                pizzaRepository.save(new Pizza("Quattro Stagioni", 100, "Tomatsås,Ost,Skinka,Champinjoner,Räkor,Kronärtskocka"));
+                pizzaRepository.save(new Pizza("MamaMia", 85, "Tomatsås,Ost,Skinka,Räkor"));
+                pizzaRepository.save(new Pizza("QuattroStagioni", 100, "Tomatsås,Ost,Skinka,Champinjoner,Räkor,Kronärtskocka"));
                 pizzaRepository.save(new Pizza("Mafia", 110, "Tomatsås,Ost,Oxfilé,Ägg,Bacon,Tabasco"));
-                pizzaRepository.save(new Pizza("Super KebabPizza", 110, "Tomatsås,Ost,Kebab,Sås,Pommes,Jalapenos,Sallad"));
+                pizzaRepository.save(new Pizza("SuperKebabPizza", 110, "Tomatsås,Ost,Kebab,Sås,Pommes,Jalapenos,Sallad"));
             }
-
         };
     }
 
